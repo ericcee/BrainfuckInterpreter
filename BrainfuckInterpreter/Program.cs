803db98a130ba5db3c0e36428faadd7b8eb9e15e
eric.cee96 <eric.cee96@gmail.com>
2021-05-25 16:09:01 +0200

    Time fix

--- BrainfuckInterpreter/Program.cs
+++ BrainfuckInterpreter/Program.cs
@@ -15,7 +15,7 @@ namespace BrainfuckInterpreter
             bfi.Run();
             TimeSpan ts = DateTime.Now - lprev; 
 
-            Console.WriteLine($"\nExecution Time: {ts.TotalMilliseconds}ms");
+            Console.WriteLine($"\nExecution Time: {ts.TotalSeconds}.{ts.Milliseconds}s");
             Console.ReadLine();
         }
     }
